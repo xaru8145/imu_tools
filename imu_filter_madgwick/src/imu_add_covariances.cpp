d8ad0d5d3749ef58e73334ecf82e6e0d8962a82d
Xavi Ruiz <xaru8145@colorado.edu>
2020-03-11 19:51:03 -0600

    minor changes, was working before

--- imu_filter_madgwick/src/imu_add_covariances.cpp
+++ imu_filter_madgwick/src/imu_add_covariances.cpp
@@ -6,9 +6,9 @@ void imuCallback(const sensor_msgs::Imu& msg)
 {
   sensor_msgs::Imu imu_msg;
   imu_msg = msg;
-  imu_msg.orientation_covariance[0] = 0.001;
-  imu_msg.orientation_covariance[4] = 0.001;
-  imu_msg.orientation_covariance[8] = 0.001;
+  imu_msg.orientation_covariance[0] = 0.00001;
+  imu_msg.orientation_covariance[4] = 0.00001;
+  imu_msg.orientation_covariance[8] = 0.00001;
 
   imu_pub.publish(imu_msg);
 }
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
 ros::init(argc, argv, "imu_add_covariances");
 ros::NodeHandle n;
 ros::Subscriber imu_sub = n.subscribe("imu/data", 1000, imuCallback);
-imu_pub = n.advertise<sensor_msgs::Imu>("imu/data_added_cov_txt", 1000);
+imu_pub = n.advertise<sensor_msgs::Imu>("imu/data_added_cov", 1000);
 ros::spin();
 
 return 0;
